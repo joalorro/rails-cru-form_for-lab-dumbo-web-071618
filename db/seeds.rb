3e6a08acfef3a0ce8f1a00a836c5c62509b8f0a5
joalorro <joalorro@gmail.com>
2018-08-14 11:38:59 -0400

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+# Artist.create name:"Drake",bio:"From canada, not a rapper (anymore)"
+# Artist.create name:"Kendrick",bio:"From comptom, an actual rapper"
+
+drake = Artist.first
+kendrick = Artist.last
+
+rap = Genre.create name:"Rap"
+not_rap = Genre.create name:"Not rap"
+
+money_trees = Song.create name:"Money Trees"
+money_trees.genre = rap
+money_trees.artist = kendrick
+
+nice_for_what = Song.create name:"nice_for_what"
+nice_for_what.genre = not_rap
+nice_for_what.artist = drake
